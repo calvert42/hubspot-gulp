e5d1a25d998d42549d820b8bc65fabb8965ca53d
Grant Foster <gfoster92@gmail.com>
2018-11-27 16:36:58 -0500

    new first commit

--- gulp/gulpfile.js
+++ gulp/gulpfile.js
@@ -1,3 +1,8 @@
+/*jslint es6 */
+'use strict';
+
+require('dotenv').config();
+
 const { src, dest, watch, parallel } = require('gulp');
 var sass = require('gulp-sass');
 var request = require('request');
@@ -6,42 +11,49 @@ var colors = require('colors');
 var prompts = require('prompts');
 
 async function init(done) {
-    let files = [
-      { 
-        title: 'Bare Minimum', 
+    var files = [
+      {
+        title: 'Bare Minimum(css)',
         value: {
           id: '6487193955',
           source: 'bare-minimum.css'
         }
       },
-      { 
-        title: 'Master', 
+      {
+        title: 'Master (css)',
         value: {
           id: '4724625226',
           source: 'master.css'
         }
+      },
+      {
+        title: 'Rapid Referral Master (html)',
+        value: {
+          id:'6488464985',
+          source: 'rapid_referral.html'
+        }
       }
     ];
-  
-    let file = await prompts(
+
+    var file = await prompts(
       {
-        type: 'select',
+        type: 'autocomplete',
         name: 'value',
         message: 'Pick a file',
         choices: files,
         initial: 0
       }
     );
-  
+
     await fs.readFile('/Users/grantfoster/Documents/sites/accelerance/' + file.value.source, 'utf-8', function read(err, data) {
       if (err) {
         throw err;
       }
-      let source = data;
-      let options = {
+      var source = data;
+      var options = {
         url: 'http://api.hubapi.com/content/api/v2/templates/' + file.value.id,
         qs: {
-          "hapikey": "46d02796-2406-40e6-8ac0-7d4027ab09ae"
+          "hapikey": process.env.HAPI_KEY
         },
         headers: {
           'User-Agent': 'request',
@@ -56,11 +68,11 @@ async function init(done) {
           console.log('error:', error);
         }
         console.log((response && response.statusCode).toString().green);
-        let date = new Date(body.updated);
-        let hours = date.getHours();
-        let minutes = "0" + date.getMinutes();
-        let seconds = "0" + date.getSeconds();
-        let formattedTime = hours + ':' + minutes.substr(-2) + ':' + seconds.substr(-2);
+        var date = new Date(body.updated);
+        var hours = date.getHours();
+        var minutes = "0" + date.getMinutes();
+        var seconds = "0" + date.getSeconds();
+        var formattedTime = hours + ':' + minutes.substr(-2) + ':' + seconds.substr(-2);
         console.log(colors.italic('Updated:' + formattedTime.toString()));
       });
     });
@@ -79,7 +91,7 @@ watch(['../sass/*.scss'], compile, function (done){
     done();
 });
 
-watch(['../*.css'], init, function (done){
+watch(['../*.css', '../*.html'], init, function (done){
     done();
 });
 
